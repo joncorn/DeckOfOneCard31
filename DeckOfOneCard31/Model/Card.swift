4144c2695baf135fbde5181f264dcb5fb501f7ec
jon <jdcorn@live.com>
2020-01-21 14:36:15 -0700

    storyboard implemented

---
+++ DeckOfOneCard31/Model/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  DeckOfOneCard31
+//
+//  Created by Jon Corn on 1/21/20.
+//  Copyright © 2020 jdcorn. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelObject: Decodable {
+    let cards: [Card]
+}
+
+class Card: Decodable {
+    let suit: String
+    let value: String
+    let image: URL
+    
+    
+}
